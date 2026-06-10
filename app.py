8754853dd5ec30ba30c3c90313b47f5911cd5808
ichinmayasahoo-cmd <ichinmayasahoo@gmail.com>
2026-06-10 10:45:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -60,4 +60,7 @@ def home():
     return render_template("index.html", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
